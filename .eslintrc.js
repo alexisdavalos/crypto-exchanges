76a18b4ed69171d554d6c0f7aad78aabe4e49ab5
Alexis <alexisdavalos.tech@gmail.com>
2022-04-03 17:08:38 +0200

    Modified eslint, prettier and tailwind configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,24 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: ["eslint:recommended", "plugin:react/recommended", "prettier"],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  plugins: ["react"],
-  ignorePatterns: [".eslintrc.*", "prettier.*"],
-  settings: {
-    react: {
-      version: "detect",
-    },
-  },
-  rules: {},
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: ["eslint:recommended", "plugin:react/recommended", "prettier"],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: "latest",
+		sourceType: "module",
+	},
+	plugins: ["react"],
+	ignorePatterns: [".eslintrc.*", "prettier.*", "tailwind.*", "*.test.js"],
+	settings: {
+		react: {
+			version: "detect",
+		},
+	},
+	rules: {
+		"react/prop-types": 0,
+	},
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	darkMode: "class",
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	purge: true,
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
