f0ebf1c8ce9eaada27e467629f00d5efc530185e
Alexis <alexisdavalos.tech@gmail.com>
2022-04-03 17:39:28 +0200

    Added React Router dependency and wrapped App in BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import DarkModeContext from "./lib/darkModeContext";
-import "./assets/css/index.css";
 import * as ReactDOMClient from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./assets/css/index.css";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
 const root = ReactDOMClient.createRoot(rootElement);
 root.render(
 	<DarkModeContext>
-		<App />
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
 	</DarkModeContext>
 );
