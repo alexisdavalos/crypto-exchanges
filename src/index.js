b2d8aa936e1f17e9d3e3984bc606210adcf7680b
Alexis <alexisdavalos.tech@gmail.com>
2022-04-03 17:13:18 +0200

    Wrapped App in DarkModeContext

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
-import "./assets/css/tailwind.css";
+import DarkModeContext from "./lib/darkModeContext";
 import "./assets/css/index.css";
 import * as ReactDOMClient from "react-dom/client";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
-
 const root = ReactDOMClient.createRoot(rootElement);
-root.render(<App />);
+root.render(
+	<DarkModeContext>
+		<App />
+	</DarkModeContext>
+);
