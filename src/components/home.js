24b99bd2004aece595f8c677f2d9548aa58eb108
Alexis <alexisdavalos.tech@gmail.com>
2022-04-04 14:46:50 +0200

    Added home level component to house main layout for home screen

---
+++ src/components/home.js
@@ -0,0 +1,45 @@
+import React from "react";
+import Banner from "./banner";
+import LogoCloud from "./logo-cloud";
+import ExchangesTable from "./exchanges-table";
+import EmailSignup from "./email-signup";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const Home = () => {
+	const [page, setPage] = useState(1);
+	const [perPage, setPerPage] = useState(10);
+	const [exchanges, setExchanges] = useState([]);
+	useEffect(() => {
+		if (page && perPage) {
+			const urlParams = new URLSearchParams({ page, per_page: perPage });
+			axios
+				.get(
+					"https://api.coingecko.com/api/v3/exchanges?" + urlParams.toString()
+				)
+				.then(({ data }) => {
+					setExchanges(data);
+				})
+				.catch((error) => {
+					console.error(error);
+					setExchanges([]);
+				});
+		}
+	}, [page, perPage]);
+	return (
+		<>
+			<Banner />
+			<LogoCloud />
+			<ExchangesTable
+				exchanges={exchanges}
+				page={page}
+				perPage={perPage}
+				setPage={setPage}
+				setPerPage={setPerPage}
+			/>
+			<EmailSignup />
+		</>
+	);
+};
+
+export default Home;
