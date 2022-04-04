db50860532013c55f9b877b7521c1d557f964a28
Alexis <alexisdavalos.tech@gmail.com>
2022-04-04 14:40:00 +0200

    Altered navbar links to only include home link

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -16,21 +16,19 @@ const Navbar = () => {
 							to="/"
 							className={({ isActive }) =>
 								isActive
-									? "text-base font-medium text-purple-600 dark:text-yellow-500"
-									: "text-base font-medium dark:text-white text-gray-500 hover:text-gray-900 dark:hover:text-gray-400"
+									? "text-base font-medium text-purple-600 dark:text-yellow-500 inline-flex items-center"
+									: "text-base font-medium dark:text-white text-gray-500 hover:text-gray-900 dark:hover:text-gray-400 inline-flex items-center"
 							}
 						>
-							Exchanges
-						</NavLink>
-						<NavLink
-							to="/coins"
-							className={({ isActive }) =>
-								isActive
-									? "text-base font-medium text-purple-600 dark:text-yellow-500"
-									: "text-base font-medium dark:text-white text-gray-500 hover:text-gray-900 dark:hover:text-gray-400"
-							}
-						>
-							Coins
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								className="h-5 w-5 mr-1"
+								viewBox="0 0 20 20"
+								fill="currentColor"
+							>
+								<path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
+							</svg>
+							Home
 						</NavLink>
 					</nav>
 				</div>
