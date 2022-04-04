0f9a31d1db340f24e6f23c3b44c7e1d8d1127bd2
Alexis <alexisdavalos.tech@gmail.com>
2022-04-04 16:31:20 +0200

    Added not-found.js functional component to display a 404 when there is no route match

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import Layout from "./components/layout";
 import Home from "./components/home";
 import Exchange from "./components/exchange";
+import NotFound from "./components/not-found";
 function App() {
 	return (
 		<Layout>
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="exchanges/:exchangeId" element={<Exchange />} />
+				<Route path="*" element={<NotFound />} />
 			</Routes>
 		</Layout>
 	);
